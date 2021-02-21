d7e19d49e670827096ce25ae31ef1dc98817508a
lyd <296821850@qq.com>
2021-02-21 20:39:09 +0800

    完成联系

--- Week 16/NOTE.md
+++ Week 16/NOTE.md
@@ -1,1 +1,26 @@
-学习笔记
+## 轮播组件 | 手势动画应用
+
+#### mouse事件
+
+- Mousestart 监听到element元素上
+- mouseover和mouseend 作用到document上
+- touchmove 和 touchend 不管触摸屏幕后，手指是在目标元素上还是滑出目标元素，target 属性的值都不变
+
+#### Symbol
+
+- ​	是一种基本数据类型 （[primitive data type](https://developer.mozilla.org/zh-CN/docs/Glossary/Primitive)）
+
+- `Symbol()`函数会返回**symbol**类型的值，该类型具有静态属性和静态方法。它的静态属性会暴露几个内建的成员对象；它的静态方法会暴露全局的symbol注册，且类似于内建对象类，但作为构造函数来说它并不完整，因为它不支持语法："`new Symbol()`"。
+
+- ```js
+  var sym1 = Symbol();
+  var sym2 = Symbol('foo');
+  var sym3 = Symbol('foo');
+  ```
+
+上面的代码创建了三个新的symbol类型。 注意，`Symbol("foo")` 不会强制将字符串 “foo” 转换成symbol类型。它每次都会创建一个新的 symbol类型：
+
+```js
+	Symbol("foo") === Symbol("foo"); // false
+```
+
---
+++ Week 16/jsx/main.js
@@ -0,0 +1,45 @@
+import { createElement } from "./framework.js"
+import { Carousel } from "./Carousel.js";
+import { List } from "./List.js";
+
+let d = [
+    {
+        img: "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
+        url:"http://www.baidu.com",
+        title:"猫咪"
+    },
+    {
+        img: "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
+        url:"http://www.baidu.com",
+        title:"猫咪2"
+    },
+    {
+        img:  "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
+        url:"http://www.baidu.com",
+        title:"猫咪3"
+    },
+    {
+        img:  "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg",
+        url:"http://www.baidu.com",
+        title:"猫咪4"
+    }
+   
+]
+
+
+// let a = <Carousel src={d} 
+// onChange={event => console.log(event.detail.position)} 
+// onClick={event => window.location.href = event.detail.data.url} 
+// />
+
+let a = <List data={d}>
+  {(record) =>
+        <div>
+            <img src={record.img} />
+            <a href={record.url}>{record.title}</a>
+        </div>
+  }
+</List>
+
+// document.body.appendChild(a)
+a.mountTo(document.body)
