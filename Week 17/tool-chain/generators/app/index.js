70b8f83fc8841f13cc9301639c08f0c89e5f54aa
lyd <296821850@qq.com>
2021-03-02 22:48:03 +0800

    完成作业

---
+++ Week 17/tool-chain/generators/app/index.js
@@ -0,0 +1,15 @@
+var  Generator = require('yeoman-generator')
+
+module.exports = class extends Generator{
+  constructor(args,opts){
+    super(args,opts)
+
+
+  }
+
+  async method1(){
+    
+  }
+  mthod2(){
+  }
+}
